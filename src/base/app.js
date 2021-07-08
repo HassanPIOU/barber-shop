28d5a527a834d3d2116cd92209db252182c04f77
Hassan PIOU <47037718+HassanPIOU@users.noreply.github.com>
2021-07-08 17:24:03 +0000

    Update app.js

--- src/base/app.js
+++ src/base/app.js
@@ -1,3 +1,3 @@
 export const APP_NAME = "Project X";
-export const SERVER_URL = "http://stanartest.co.uk/web-api-barber-shop/public/api/";
+export const SERVER_URL = "https://stanartest.co.uk/web-api-barber-shop/public/api/";
 export  const GoogleMapsAPI  = "AIzaSyDR5-GEgHP3kHq7bPnMOZJDpe7D8BC9CUM"
